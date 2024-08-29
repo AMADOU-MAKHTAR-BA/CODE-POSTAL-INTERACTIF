4355a3d01f4ce6318094903d80f1741dd21c0f7d
developeur123@gmail.com <developeur123@gmail.com>
2024-08-29 23:08:19 +0000

    MON CODE POSTAL INTERACTIF

---
+++ main.js
@@ -0,0 +1,40 @@
+
+var div = document.createElement('div');
+      div.id = 'div';
+  div.style.margin = '10px 0 0 0'
+document.body.appendChild(div);
+
+
+const cp = document.querySelector('#cp');
+
+cp.addEventListener('input', function(){
+  if (this.value.length == 5) {
+    let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre` ;
+
+fetch(url).then(response=>response.json()
+.then(données => {
+  console.log(données);
+     
+     let affichage = '<ul>';
+        
+ for(let donnée of données){
+   affichage += `<li><strong>${donnée.nom}</strong>  - ${donnée.population} habitants</li>`
+ }
+        affichage +='</ul>'
+  div.innerHTML = affichage;
+      })).catch(erreur =>
+  div.innerHTML = 'erreur ' +  erreur 
+      )
+      
+    
+  }
+  else{
+    div.innerHTML = '';
+  }
+});
+
+    console.log('76370');
+
+ 
+   
+
